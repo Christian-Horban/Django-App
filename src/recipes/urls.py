bd3f2218bab9f309c0432430d1cfa8c22ce205ce
Git-Xian831 <christian.k.horban@gmail.com>
2023-11-16 19:15:52 -0700

    2.5 deliverables
    Created additional views, updated models and data organization, updated styling to improve UX

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("recipes/", views.recipe_overview, name="recipe_overview"),
+    path("recipes/<int:pk>/", views.recipe_detail, name="recipe_detail"),
+    path("search/", views.search_results, name="search_results"),
 ]
